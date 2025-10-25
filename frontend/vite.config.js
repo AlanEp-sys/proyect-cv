e84ef61b37faf8abfcc0a43ad0aa5cb85a8d63ff
AlanEp-sys <espinozaperezalan@gmail.com>
2025-10-24 19:12:57 -0700

    tercer Commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// Cambia 'tu-repo' por el nombre de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/AlanEp-sys/proyect-cv/'
+  base: '/proyect-cv/'
 })
